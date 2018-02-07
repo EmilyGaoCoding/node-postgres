97d7e69c5d5fc0c5937cb8c04650f13b5f3c8e79
Emily Gao <emily.s.gao@gmail.com>
2018-02-06 21:59:22 -0700

    created knexfile.js

--- connect_pg.js
+++ connect_pg.js
@@ -1,15 +1,6 @@
 const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
-// const client = new pg.Client({
-//   user     : settings.user,
-//   password : settings.password,
-//   database : settings.database,
-//   host     : settings.hostname,
-//   port     : settings.port,
-//   ssl      : settings.ssl
-// });
-
 var knex = require('knex')({
   client: 'pg',
   connection: {
@@ -21,7 +12,5 @@ var knex = require('knex')({
     ssl      : settings.ssl
   }
 });
-
-// client.connect();
 
 module.exports = knex;
